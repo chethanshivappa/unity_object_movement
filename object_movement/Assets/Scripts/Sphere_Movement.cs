e1b600d0507b78559eb4291bfadd3df835b07a84
Unknown <unknown@example.com>
2020-05-24 07:51:52 +0530

    Added Sphere_Movement_Script
    Attached a script to sphere

---
+++ object_movement/Assets/Scripts/Sphere_Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere_Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    public GameObject go;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("this sphere is meant to move");
+        
+        rb = GetComponent<Rigidbody>();
+        print(rb.useGravity);
+        print(go.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
